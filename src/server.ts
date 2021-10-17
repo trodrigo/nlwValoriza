363847b2c47faff61f5fb9c11a51826189c48727
Rodrigo <tr.rodrigo@gmail.com>
2021-10-17 20:13:07 -0300

    Create Tags

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
-import express, { request, response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { router } from "./routes";
 
 import "./database"
@@ -7,7 +9,22 @@ import "./database"
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "erro",
+        message: "Internal Server Error"
+    });
+
+});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server running NLW"));
